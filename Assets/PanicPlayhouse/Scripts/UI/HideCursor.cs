876b36c5119fa61ee115db4b030746df75767fac
Gabriela Matias <jasbrela@gmail.com>
2023-05-13 13:24:21 -0300

    hide cursor in game scene

---
+++ Assets/PanicPlayhouse/Scripts/UI/HideCursor.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace PanicPlayhouse.Scripts.UI
+{
+    public class HideCursor : MonoBehaviour
+    {
+        void Start()
+        {
+            Cursor.visible = false;
+        }
+    }
+}
