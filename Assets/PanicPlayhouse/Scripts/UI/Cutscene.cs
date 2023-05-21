697a4ac60dc9a1ea79e95bef33105d1a6e519cb8
Gabriela Matias <jasbrela@gmail.com>
2023-05-21 12:08:17 -0300

    add more bindings to interaction

---
+++ Assets/PanicPlayhouse/Scripts/UI/Cutscene.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PanicPlayhouse.Scripts.UI
+{
+    public class Cutscene : MonoBehaviour
+    {
+        [SerializeField] private List<Image> images;
+    }
+}
