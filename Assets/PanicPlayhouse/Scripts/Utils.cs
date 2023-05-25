543ed4f5ff88e265f52e91e7dcdcb6ecf12d60af
FeVergara <fevergara2002@gmail.com>
2023-05-25 12:41:36 -0300

    Level & Balanceamento final

--- Assets/PanicPlayhouse/Scripts/Utils.cs
+++
@@ -1,8 +0,0 @@
-namespace PanicPlayhouse.Scripts
-{
-    public static class Utils
-    {
-        public static string Bold(this string str) => "<b>" + str + "</b>";
-        public static string Color(this string str, string color) => $"<color={color}>{str}</color>";
-    }
-}
