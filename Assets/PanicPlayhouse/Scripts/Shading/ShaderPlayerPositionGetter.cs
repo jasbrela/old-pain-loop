34b98fa29d2de9ae9872af2658a53add2531eb28
João Magalhães (Jaum) <dragon530698@gmail.com>
2023-07-09 08:22:19 -0300

    GD Feedback changes

--- Assets/PanicPlayhouse/Scripts/Shading/ShaderPlayerPositionGetter.cs
+++ Assets/PanicPlayhouse/Scripts/Shading/ShaderPlayerPositionGetter.cs
@@ -7,14 +7,29 @@ namespace PanicPlayhouse.Scripts.Shading
 {
     public class ShaderPlayerPositionGetter : MonoBehaviour
     {
-        public PlayerMovement player;
-        public List<Material> materialsToUpdate = new List<Material>();
+        [SerializeField] PlayerMovement player;
+        [SerializeField] List<Material> materialsToUpdate = new List<Material>();
+
+        private Vector3 playerOriginalSceneLocation = Vector3.zero;
+
+        void Awake()
+        {
+            playerOriginalSceneLocation = player.transform.position;
+        }
 
         void Update()
         {
             foreach (Material material in materialsToUpdate)
             {
                 material.SetVector("_PlayerPosition", player.transform.position);
+            }
+        }
+
+        void OnDestroy()
+        {
+            foreach (Material material in materialsToUpdate)
+            {
+                material.SetVector("_PlayerPosition", playerOriginalSceneLocation);
             }
         }
     }
